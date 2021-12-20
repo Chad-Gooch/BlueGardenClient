82b76463a1dad0c12216a2b570a9ac5ea508b75a
Brayden Bridgeman <BraydenBridgeman@gmail.com>
2021-12-20 11:21:06 -0500

    added functionality to AddPlant

--- src/mygarden/AddPlant.js
+++ src/mygarden/AddPlant.js
@@ -1,19 +1,42 @@
-import React, {useState, useEffect} from 'react';
-import {Table} from 'reactstrap';
+import React from 'react';
+import {Button} from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './AddPlant.css';
 
 const AddPlant = props => {
+    
+    let berryAdd = (id) => {
+        let arrayId = id - 1;
+        let detail = props.item[arrayId];
+        let plant = props.berryHolder[arrayId];
+        console.log(detail, plant)
+        // fetch method
+        fetch('http://localhost:5000/add/', {
+            method: 'POST',
+            body: JSON.stringify({
+                plantName:plantName,
+                species:species,
+                plantImage:plantImage,
+                season:season,
+                owner_id: idNumber
+            }),
+            headers: new Headers ({
+                'Content-Type': 'application/json'
+            })
+        }).then(
+            (response) => response.json()
+        ).then((data) => {
+            props.berryToAdd(data.berryHolder);
+            console.log(data.berryHolder)
+        })
+    }
 
-    const [settingOne, setSettingOne] = useState('');
-
-    useEffect(()=>{
-    },[])
 
     return (
-        <Table>AddPlant{settingOne}
-        </Table>
-    );
+        <Button className="addToGarden" onClick={() => berryAdd(props.berryToAdd)}>
+            Add to My Garden
+        </Button>
+    )
 }
 
 export default AddPlant;
